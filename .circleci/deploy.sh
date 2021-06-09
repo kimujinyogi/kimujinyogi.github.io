b831804a97bacd902865a3020c92188d0f2ce25c
JinHyuck Kim <kimujinyogi@gmail.com>
2021-06-09 14:19:17 +0900

    test

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -5,13 +5,19 @@ set -e
 
 DEPLOY_DIR=deploy
 
+echo j_kim buildstart
+
 # gitの諸々の設定
 git config --global push.default simple
 git config --global user.email kimujinyogi_circle@gmail.com
 git config --global user.name $CIRCLE_USERNAME
 
+echo j_kim config end
+
 # gh-pagesブランチをdeployディレクトリにクローン
 git clone -q --branch=campcar $CIRCLE_REPOSITORY_URL $DEPLOY_DIR
+
+echo j_kim clone end
 
 echo ($ls)
 
